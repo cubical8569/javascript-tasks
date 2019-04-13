f7373de0ce37349c76807447402b572d3d759a61
CookiesDeathCookies <azotx12345@gmail.com>
2019-04-13 21:50:57 +0300

    lessons

---
+++ ifelse/2/script.js
@@ -0,0 +1,5 @@
+"use strict";
+
+let answer = prompt("What's the official name of Javascript?")
+
+alert(answer == "ECMAScript" ? "Right" : "You don't know?\n\"ECMAScript\"") 
---
+++ ifelse/3/script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+let num = prompt()
+
+if (num > 0) {
+	alert(1);
+} else if (num == 0) {
+	alert(0);
+} else {
+	alert(-1);
+}
