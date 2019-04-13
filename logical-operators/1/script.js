f4a5244690abf9daced2d07b91fce1bae4418c8b
CookiesDeathCookies <azotx12345@gmail.com>
2019-04-13 23:40:39 +0300

    logical operators

---
+++ logical-operators/1/script.js
@@ -0,0 +1,10 @@
+// "use strict";
+
+let name = prompt("Who's there?")
+
+if (name === "Admin") {
+	let password = prompt("Password");	
+	
+} else if (name !== '' && name !== null) {
+	alert("I don't know ya");
+} 
